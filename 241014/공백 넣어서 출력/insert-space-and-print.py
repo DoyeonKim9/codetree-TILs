9126daa0a7ae13168e23d27cd55544fec5104e47
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-14 01:06:48 +0900

    Title: 공백 넣어서 출력, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/공백 넣어서 출력/insert-space-and-print.py
+++ 241014/공백 넣어서 출력/insert-space-and-print.py
@@ -1,5 +1,5 @@
 print("score\t\tsubject\t\tname")
 print("")
-print("  100\t\t   math\t\t sam")
+print("  100\t\t   math\t\tsam")
 print("   88\t\tscience\t\tpaul")
-print("   96\t       language\t\tjack")
+print("   96\t       language\t\t jack")
