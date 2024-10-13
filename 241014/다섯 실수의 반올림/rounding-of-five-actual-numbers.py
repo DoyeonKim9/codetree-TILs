e182928224ed33af779117d010328805f38621a3
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-14 01:22:07 +0900

    Title: 다섯 실수의 반올림, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241014/다섯 실수의 반올림/rounding-of-five-actual-numbers.py
@@ -0,0 +1,11 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+
+print(f"{a:.3f}")
+print(f"{b:.3f}")
+print(f"{c:.3f}")
+print(f"{d:.3f}")
+print(f"{e:.3f}")
