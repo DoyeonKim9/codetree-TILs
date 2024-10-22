a9d80574bb6f7496a3f97bb3a386b59eca7619f1
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-23 01:21:05 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/삼항연산자/ternary-operator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    if (a == 100) {
+        printf("pass");
+    }
+    else {
+        printf("failure");
+    }
+    return 0;
+}
