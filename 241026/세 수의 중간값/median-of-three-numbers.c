b3dddd0c427c3e9e1c13eb1c0b9e8830a52a43b7
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-26 19:22:23 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241026/세 수의 중간값/median-of-three-numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (b > a && b < c) {
+        printf("1");
+    }
+    else {
+        printf("0");
+    }
+    return 0;
+}
