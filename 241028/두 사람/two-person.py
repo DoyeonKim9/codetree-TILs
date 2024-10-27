843537c8d75da38994d44b1db82a79f83228aa6f
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-28 00:46:01 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241028/두 사람/two-person.py
@@ -0,0 +1,11 @@
+arr1 = input().split()
+a1 = int(arr1[0])
+b1 = str(arr1[1])
+arr2 = input().split()
+a2 = int(arr2[0])
+b2 = str(arr2[1])
+
+if (a1 >= 19 or b1 >= 19) and (b1 == 'M' or b2 == 'M'):
+    print("1")
+else:
+    print("0")
