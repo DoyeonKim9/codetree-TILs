b4b169e5adefe42bd00a1977bde168eb434e29e6
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-28 00:30:59 +0900

    Title: 장학금, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241028/장학금/scholarship.py
+++ 241028/장학금/scholarship.py
@@ -2,9 +2,9 @@ mid, fin = map(int, input().split())
 
 if mid >= 90:
     if fin >= 95:
-        print("10")
+        print("100000")
     elif fin >= 90:
-        print("5")
+        print("50000")
     else:
         print("0")
 else:
