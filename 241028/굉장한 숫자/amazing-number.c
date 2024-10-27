dc0a08d34dae71868e27bc1262bac48d339af6c0
DoyeonKim9 <ehdus9809@gmail.com>
2024-10-28 02:09:14 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241028/굉장한 숫자/amazing-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if ((n % 2 == 1 && n % 3 == 0) || (n % 2 == 0 && n % 5 == 0)) {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    return 0;
+}
