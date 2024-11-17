b0bec3e3e612ac0c002df6afa36bcdcf87d1e506
DoyeonKim9 <ehdus9809@gmail.com>
2024-11-18 03:52:46 +0900

    Title: 출석 부르기 2, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241118/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,17 @@
+while True:
+    n = int(input())
+    if n == 1:
+        print("John")
+        continue
+    if n == 2:
+        print("Tom")
+        continue
+    if n == 3:
+        print("Paul")
+        continue
+    if n == 4:
+        print("Sam")
+        continue
+    else:
+        print("Vacancy")
+        break
