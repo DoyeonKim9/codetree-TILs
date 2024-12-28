fe10e3f990bc67160167ee08c4f3c63fb4540dbf
DoyeonKim9 <ehdus9809@gmail.com>
2024-12-29 02:28:24 +0900

    Title: 소수 판별기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241229/소수 판별기/prime-number-discriminator.py
+++ 241229/소수 판별기/prime-number-discriminator.py
@@ -1,10 +1,10 @@
 n = int(input())
 satis = True
 
-for i in range(1, n):
+for i in range(2, n):
     if n % i == 0:
         satis = False
 if satis == True:
-    print("C")
-else:
     print("P")
+else:
+    print("C")
