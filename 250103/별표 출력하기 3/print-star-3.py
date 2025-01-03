b2e5b7c5dc494e8487973fdee3c55e43d286c94c
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-03 17:59:28 +0900

    Title: 별표 출력하기 3, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250103/별표 출력하기 3/print-star-3.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(i):
+        print(" ", end = " ")
+        
+    for j in range((2*n)-(2*i)-1):
+        print("*", end = " ")
+    print()
+    
+    
