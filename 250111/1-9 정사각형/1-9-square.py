e7fc7479b3ecf2cc930c6893f51ace389082911d
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-11 16:30:30 +0900

    Title: 1-9 정사각형, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250111/1-9 정사각형/1-9-square.py
@@ -0,0 +1,11 @@
+n = int(input())
+cnt = 1
+for i in range(n):
+    for j in range(n):
+        print(cnt, end="")
+        cnt += 1
+        if  cnt == 10:
+            cnt = 1
+        
+        
+    print()
