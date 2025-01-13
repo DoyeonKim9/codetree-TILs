d6082e7e7dadf64dae422592b7fb3f694c30f4ba
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-13 16:47:02 +0900

    Title: 상하로 반복하며 출력, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250113/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if j % 2 == 0:
+            print(i+1, end="")
+        else:
+            print(n-i, end="")
+        print() 
