db121afc3f4051f3d5dd670c81b6cac28e47a9da
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-14 18:56:48 +0900

    Title: 알파벳 출력하기 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250114/알파벳 출력하기 2/print-alphabet-2.py
+++ 250114/알파벳 출력하기 2/print-alphabet-2.py
@@ -3,7 +3,9 @@ cnt = 'A'
 for i in range(n):
     for j in range(i):
         print(" ", end =" ")
-    for j in range(n-i, 0, -1):
+    for j in range(n-i):
         print(cnt, end = " ")
         cnt = chr(ord(cnt) + 1)
+        if ord(cnt) > ord('Z'):
+            cnt = 'A'
     print()
