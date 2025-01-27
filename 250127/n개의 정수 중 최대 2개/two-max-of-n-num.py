9c8845de765144b9c88f4b88bd48907a7b914956
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-27 14:35:11 +0900

    Title: n개의 정수 중 최대 2개, Time: 82ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -1,8 +1,8 @@
 n = int(input())
 arr = list(map(int, input().split()))
-max1, max2 = arr[0], arr[1]
 
-if max1 > max2:
+
+if arr[0]> arr[1]:
     max1, max2 = arr[0], arr[1]
 else:
     max1, max2 = arr[1], aee[0]
