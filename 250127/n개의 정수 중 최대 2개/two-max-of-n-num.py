1c195dd87363ca95003c7007a6521c469685f255
DoyeonKim9 <ehdus9809@gmail.com>
2025-01-27 14:37:15 +0900

    Title: n개의 정수 중 최대 2개, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250127/n개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -1,14 +1,22 @@
+# 변수 선언 및 입력
 n = int(input())
 arr = list(map(int, input().split()))
 
+# Step 1: 처음 2개의 원소 중 더 큰 값을 max1에
+#                        더 작은 값을 max2에 넣습니다.
 if arr[0] > arr[1]:
     max1, max2 = arr[0], arr[1]
 else:
-    max1, max2 = arr[1], aee[0]
+    max2, max1 = arr[0], arr[1]
 
+# Step 2: 3번째 원소부터 보면서 max1과 max2를 갱신합니다.
 for i in range(2, n):
     if arr[i] >= max1:
-        max1, max2 = arr[i], max1
+        # Case 1 : 지금까지 본 숫자들보다 좋다면
+        #          max2, max1 모두 갱신해줍니다.
+        max2, max1 = max1, arr[i]
     elif arr[i] > max2:
+        # Case 2 : max2보다만 좋다면 max2를 갱신합니다.
         max2 = arr[i]
+
 print(max1, max2)
